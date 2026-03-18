7b0a8fb30b064e002dd3cb8bf477a9a7f8755f04
hermaeusmora <aleshka.dev@gmail.com>
2026-03-18 20:19:58 +0200

    add basic project settings

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  sassOptions: {
+    implementation: 'sass',
+  },
 };
 
 export default nextConfig;
