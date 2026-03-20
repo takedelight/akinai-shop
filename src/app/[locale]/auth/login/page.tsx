57f778ad674ee8ad8df1c2914301fa9b7e4f587b
hermaeusmora <aleshka.dev@gmail.com>
2026-03-20 19:39:52 +0200

    setup next-auth with Google OAuth provider

--- src/app/[locale]/auth/login/page.tsx
+++ src/app/[locale]/auth/login/page.tsx
@@ -1,24 +1,38 @@
-import { getTranslations } from 'next-intl/server';
+'use client';
 
+import { signIn } from 'next-auth/react';
+import { useTranslations } from 'next-intl';
+
+import { Card } from '@mantine/core';
 import { RiGoogleFill } from '@remixicon/react';
 
 import { Link } from '@/shared/configs/i18n';
 
 import s from './styles.module.scss';
 
-const LoginPage = async () => {
-  const t = await getTranslations('auth');
+const LoginPage = () => {
+  const t = useTranslations('auth');
 
   return (
     <section className={s.auth}>
-      <ul className={s.oauthLinks}>
-        <li className={s.oauthItem}>
-          <Link href="/" className={s.oauthLink}>
-            <RiGoogleFill />
-            {t('OAuth.google')}
-          </Link>
-        </li>
-      </ul>
+      <Card withBorder>
+        <h2 className={s.title}>{t('OAuth.title')}</h2>
+        <ul className={s.oauthLinks}>
+          <li className={s.oauthItem}>
+            <Link
+              href="/auth/login"
+              onClick={async (e) => {
+                e.preventDefault();
+                await signIn('google', { callbackUrl: '/' });
+              }}
+              className={s.oauthLink}
+            >
+              <RiGoogleFill />
+              {t('OAuth.google')}
+            </Link>
+          </li>
+        </ul>
+      </Card>
     </section>
   );
 };
