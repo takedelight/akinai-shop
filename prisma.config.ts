4751119269c98b7bc64a71f7aa15655f57953268
hermaeusmora <aleshka.dev@gmail.com>
2026-03-20 10:46:47 +0200

    add model for database; add prisma.seed for create admin user;

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'prisma/config';
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
+    seed: 'tsx ./src/shared/lib/prisma/prisma.seed.ts',
     path: 'prisma/migrations',
   },
   datasource: {
